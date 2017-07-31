4200ba86182398babe405d972cf8b284594988e2
jarec <ormus395@gmail.com>
2017-07-31 13:59:28 -0700

    Updated models

--- models/models_employers.js
+++ models/models_employers.js
@@ -1,8 +1,22 @@
 var mongoose = require ('mongoose');
 var Schema = mongoose.Schema;
 
+
+// Subject to change
 var Employers = new Schema({
+
   firstname: String,
   lastname: String,
+  email: String,
+  company: String,
+  phone: String,
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zipcode
+  },
+  password: String
+
 
 })
--- models/models_users.js
+++ models/models_users.js
@@ -1,8 +1,22 @@
 var mongoose = require ('mongoose');
 var Schema = mongoose.Schema;
 
+
+// Subject to change
 var Users = new Schema({
+
+  username: String,
   firstname: String,
   lastname: String,
-  
+  email: String,
+  phone: String,
+  adress: {
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String
+  },
+  github: String,
+  password: String
+
 })
