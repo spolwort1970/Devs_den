a692494dc94f7782f0918ade43b463d38bcec669
jarec <ormus395@gmail.com>
2017-08-21 15:47:29 -0700

    added employer profile route

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cloudinary = require('cloudinary');
+// const cloudinary = require('cloudinary');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
